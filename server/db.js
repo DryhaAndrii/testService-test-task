213d68c10ca09f408b05a841ae67f6298a072c76
Dryha Andrii <driga.and@gmail.com>
2025-09-05 18:32:07 +0300

    qweqwe

--- server/db.js
+++ server/db.js
@@ -1,16 +1,25 @@
-const mongoose = require('mongoose');
-require('dotenv').config(); 
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+    serverSelectionTimeoutMS: 10000,
+  })
+  .then(() => {
+    console.log("MongoDB connection success");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('MongoDB connection success');
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connection success");
 });
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-// server/server.js
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+console.log("ENV:", process.env.PORT);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', require('./routes/api')); // Подключение маршрутов
+app.use("/api", require("./routes/api"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
